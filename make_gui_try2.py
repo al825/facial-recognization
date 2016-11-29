6306349f7108e888c5a11ecdf256bdc0e0b7a9a3
al825 <anqi.liang@yale.edu>
2016-11-28 23:01:35 -0800

    Can display a second button after

--- make_gui_try2.py
+++ make_gui_try2.py
@@ -2,28 +2,25 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 
-class BuckyButtons:
-    def __init__(self, master):
-        self.master = master
-        self.frame = Frame(master)
-        self.frame.pack()
-        
-        self.button1 = Button(self.frame, text='Print', command=self.shownext)
-        self.button1.pack(side=LEFT)
-        
 
         
-    def shownext(self):
-        self.frame.pack_forget()
-        self.frame2 = Frame(self.master)
-        self.frame2.pack()
-        image = Image.open(r"dan4.jpg")
-        photo = ImageTk.PhotoImage(image)
-        self.label1 = Label(self.frame2, image=photo) # photo needs to be on a label
-        self.label1.pack()
+def shownext():
+    frame.pack_forget()
+    frame2 = Frame(root, bg='red')    
+    frame2.pack()
+    button2 = Button(frame2, text='yes')
+    button2.pack()
+    #image = Image.open(r"dan4.jpg")
+    #photo = ImageTk.PhotoImage(image)
+    #label1 = Label(frame2, image=photo) # photo needs to be on a label
+    #label1.pack()
 
 
 if __name__ == '__main__':        
     root = Tk()
-    bb = BuckyButtons(root)
+    frame = Frame(root, bg='blue')
+    frame.pack(fill=X)
+    button1 = Button(frame, text='print', command=shownext)
+    button1.pack()
+
     root.mainloop()
