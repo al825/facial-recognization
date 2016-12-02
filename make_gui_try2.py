8594416b228c46dd142fae84ef515b1b7bdf7978
al825 <anqi.liang@yale.edu>
2016-12-02 00:03:31 -0800

    Avoid global import

--- make_gui_try2.py
+++ make_gui_try2.py
@@ -1,14 +1,13 @@
-from tkinter import *
+import tkinter as tk
 
-
-class EyeCenterApp(Tk):
+class EyeCenterApp(tk.Tk):
     def __init__(self, height, width):
-        Tk.__init__(self)
+        tk.Tk.__init__(self)
         self.geometry('{}x{}'.format(height, width))
         self.wm_title('Eye Center Prediction')
         self.resizable(width=False, height=False)
-        self.container = Frame(self)
-        self.container.pack(side = TOP, fill=BOTH, expand=True)
+        self.container = tk.Frame(self)
+        self.container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)        
         self.frames = {}
@@ -22,19 +21,19 @@ class EyeCenterApp(Tk):
     def show_frame(self, page):
         self.frames[page].tkraise()
         
-class StartPage(Frame):
+class StartPage(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         self.controller = controller
         self.create_widgets()
     
     def create_widgets(self): 
-        self.instruction = Label(self, text='Facial Recognization')
+        self.instruction = tk.Label(self, text='Facial Recognization')
         self.instruction.config(font=("Courier", 20))
-        self.instruction.place(relx=0.5, rely=0.3, anchor=CENTER)
-        self.button = Button(self, text = 'START', command=self.click_start)
-        self.button.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.instruction.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        self.button = tk.Button(self, text = 'START', command=self.click_start)
+        self.button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
      
     def click_start(self):
         self.controller.show_frame(PageOne)
@@ -42,30 +41,30 @@ class StartPage(Frame):
         #self.controlller.show_frame(PageTwo)
                     
         
-class PageOne(Frame):
+class PageOne(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         self.controller = controller
         self.create_widgets()
     
     def create_widgets(self): 
-        self.label = Label(self,text='Creating Model')
+        self.label = tk.Label(self,text='Creating Model')
         self.label.config(font=("Courier", 30))
-        self.label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
      
         
-class PageTwo(Frame):
+class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         self.controller = controller
         self.create_widgets()
         
     def create_widgets(self):
-        self.label = Label(self, text='Predict')
+        self.label = tk.Label(self, text='Predict')
         self.label.config(font=("Courier", 30))
-        self.label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
     
     
         
