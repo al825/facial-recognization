fdef6248ecd61d6bb0f14104f7006540cfdc39a8
al825 <anqi.liang@yale.edu>
2016-12-02 22:28:05 -0800

    Find the original best_model back.
    The file was accidentally changed.

--- best_model.py
+++ best_model.py
@@ -8,90 +8,6 @@ import my_func
 import time
 from eye_identifier import EyeCenterIdentifier, GridSearch
 from image_preprocess import imanorm, histeq, imaderiv
-from sklearn.ensemble import RandomForestClassifier
-
-
-class BestModel():
-    SIZE = 96
-    HEIGHT = 12
-    WIDTH = 20
-    half_HEIGHT = 6
-    half_WIDTH = 10
-    N_sub = 200
-    N_plots = 20
-    
-    def __init__(self):
-        
-        
-    def build_model(self):
-    # Import data
-    data_ori = pd.read_csv(r"..\data\training.csv")
-
-    # use a subset of the data 
-    
-    data = data_ori.iloc[:BestModel.N_sub]
-    images = data.Image.map(my_func.str_split) # Transfer Image into arrays
-    data = data.drop('Image', 1)    
-    data_pos = data[['left_eye_center_x', 'left_eye_center_y', 'right_eye_center_x', 'right_eye_center_y']]
-
-
-    # Remove rows with nan positions
-    nan_index = data_pos.index[data_pos.isnull().any(axis=1)]
-    images = images.drop(nan_index, axis=0)
-    data = data.drop(nan_index, axis=0)
-    data_pos = data_pos.drop(nan_index, axis=0)
-
-    # histeq transform
-    images = images.apply(histeq)
-    #images = images.apply(imaderiv)
-
-    # Split the data into training set and testing set
-    from sklearn.model_selection import train_test_split
-    images_train, images_test, data_pos_train, data_pos_test = train_test_split(images, data_pos, test_size = 0.2, random_state = 312)
-
-    # Get 20 subplots from each image, 1 right eye, 1 left eye, 2 randomly selected subplots
-    # Create the eye training data set
-    random.seed(123)
-    col_names = ['pixel' + str(v) for v in range(0, HEIGHT * WIDTH)] + ['center_X', 'center_Y', 'is_eye']
-    data_eye = pd.DataFrame(columns = col_names)   
-
-    for i in range(0, images_train.shape[0]):
-        t1=time.time()
-        center_X = np.empty(0)
-        center_Y = np.empty(0)
-        is_eye = np.empty(0)
-        # Select the two eye subplots
-        for _eye in ['left_eye_center', 'right_eye_center']:
-            _eye_x = _eye + '_x'
-            _eye_y = _eye + '_y'
-            _x = data_pos_train.iloc[i][ _eye_x]
-            _y = data_pos_train.iloc[i][ _eye_y]
-            _x = np.array([_x-2, _x, _x+2, _x, _x])
-            _y = np.array([_y, _y, _y, _y-1, _y+1])
-            center_X = np.append(center_X, _x)
-            center_Y = np.append(center_Y, _y)
-            is_eye = np.append(is_eye, [1] * int(N_plots / 4))
-        # randomly select two subplots
-        for r in range(int(N_plots / 2)):
-            while True:
-                _x = random.uniform(0, SIZE)
-                _y = random.uniform(0, SIZE)
-                # do not want the random center to be too close to the eyes
-                if not (abs(_x - data_pos_train.iloc[i][ 'left_eye_center_x']) + abs(_y - data_pos_train.iloc[i][ 'left_eye_center_y']) < HEIGHT + WIDTH or abs(_x - data_pos_train.iloc[i][ 'right_eye_center_x']) + abs(_y - data_pos_train.iloc[i][ 'right_eye_center_y']) < HEIGHT + WIDTH):
-                    break
-            center_X = np.append(center_X, _x)
-            center_Y = np.append(center_Y, _y)
-            is_eye = np.append(is_eye, 0)
-
-        for j in range (0,len(center_X)):            
-            temp = my_func.cut_image(center_X[j], center_Y[j], half_WIDTH, half_HEIGHT)    
-            ima = pd.Series(images_train.iloc[i][temp[1]])
-            ima = ima.append(pd.Series([center_X[j], center_Y[j], is_eye[j]]))
-            ima.index = col_names
-            data_eye = data_eye.append(ima, ignore_index = True)           
-        
-    def make_prediction(self):
-
 
 # Set up global parameters
 global SIZE
@@ -100,7 +16,82 @@ global WIDTH
 global half_HEIGHT 
 global half_WIDTH
 global N_plots 
+SIZE = 96
+HEIGHT = 12
+WIDTH = 20
+half_HEIGHT = 6
+half_WIDTH = 10
 
+# Import data
+data_ori = pd.read_csv(r"..\data\training.csv")
+
+# use a subset of the data 
+N_sub = 200
+data = data_ori.iloc[:N_sub]
+images = data.Image.map(my_func.str_split) # Transfer Image into arrays
+data = data.drop('Image', 1)    
+data_pos = data[['left_eye_center_x', 'left_eye_center_y', 'right_eye_center_x', 'right_eye_center_y']]
+
+
+# Remove rows with nan positions
+nan_index = data_pos.index[data_pos.isnull().any(axis=1)]
+images = images.drop(nan_index, axis=0)
+data = data.drop(nan_index, axis=0)
+data_pos = data_pos.drop(nan_index, axis=0)
+
+# histeq transform
+images = images.apply(histeq)
+#images = images.apply(imaderiv)
+
+# Split the data into training set and testing set
+from sklearn.model_selection import train_test_split
+images_train, images_test, data_pos_train, data_pos_test = train_test_split(images, data_pos, test_size = 0.2, random_state = 312)
+print ("The training set has {} observations and the testing set has {} observations". format(images_train.shape[0], images_test.shape[0]))
+
+
+# Get 20 subplots from each image, 1 right eye, 1 left eye, 2 randomly selected subplots
+N_plots = 20
+
+# Create the eye training data set
+random.seed(123)
+col_names = ['pixel' + str(v) for v in range(0, HEIGHT * WIDTH)] + ['center_X', 'center_Y', 'is_eye']
+data_eye = pd.DataFrame(columns = col_names)   
+
+for i in range(0, images_train.shape[0]):
+    t1=time.time()
+    center_X = np.empty(0)
+    center_Y = np.empty(0)
+    is_eye = np.empty(0)
+    # Select the two eye subplots
+    for _eye in ['left_eye_center', 'right_eye_center']:
+        _eye_x = _eye + '_x'
+        _eye_y = _eye + '_y'
+        _x = data_pos_train.iloc[i][ _eye_x]
+        _y = data_pos_train.iloc[i][ _eye_y]
+        _x = np.array([_x-2, _x, _x+2, _x, _x])
+        _y = np.array([_y, _y, _y, _y-1, _y+1])
+        center_X = np.append(center_X, _x)
+        center_Y = np.append(center_Y, _y)
+        is_eye = np.append(is_eye, [1] * 5)
+    # randomly select two subplots
+    for r in range(10):
+        while True:
+            _x = random.uniform(0, SIZE)
+            _y = random.uniform(0, SIZE)
+            # do not want the random center to be too close to the eyes
+            if not (abs(_x - data_pos_train.iloc[i][ 'left_eye_center_x']) + abs(_y - data_pos_train.iloc[i][ 'left_eye_center_y']) < HEIGHT + WIDTH or abs(_x - data_pos_train.iloc[i][ 'right_eye_center_x']) + abs(_y - data_pos_train.iloc[i][ 'right_eye_center_y']) < HEIGHT + WIDTH):
+                break
+        center_X = np.append(center_X, _x)
+        center_Y = np.append(center_Y, _y)
+        is_eye = np.append(is_eye, 0)
+
+    for j in range (0,len(center_X)):            
+        temp = my_func.cut_image(center_X[j], center_Y[j], half_WIDTH, half_HEIGHT)    
+        ima = pd.Series(images_train.iloc[i][temp[1]])
+        ima = ima.append(pd.Series([center_X[j], center_Y[j], is_eye[j]]))
+        ima.index = col_names
+        data_eye = data_eye.append(ima, ignore_index = True)           
+    print("Iteration {} used {:.2f} seconds".format(i, time.time()-t1))
 
        
 
@@ -131,6 +122,8 @@ mse_mean = eye_bench.get_mse(pred_data_mean, test_pos) # bench mark: 2.96
 step_size = (1, 1)
 N_steps = (8, 4)
 
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
 
 
 clf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
